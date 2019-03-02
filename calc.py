fe73fad555f346d6910365adbc7c92c1e8b0f3f1
Abhishek Dhulipudi <abhishek.dhulipudi@autodesk.com>
2019-03-02 20:15:10 +0800

    handling ZeroDivisionError

--- calc.py
+++ calc.py
@@ -15,7 +15,10 @@ args = parser.parse_args()
 def calculator():
 	# Reduce function iterates over the list by taking values as tuples of two,
 	# performs the lambda expression, takes the result as the first value of the next tuple.
-	res = reduce((lambda x, y: op_map[args.operation](x, y)), args.numbers)
-	print('Result for {} on {} is'.format(args.operation, args.numbers), res)
+	try:
+		res = reduce((lambda x, y: op_map[args.operation](x, y)), args.numbers)
+		print('Result for {} on {} is'.format(args.operation, args.numbers), res)
+	except ZeroDivisionError:
+		print('Doesnt allow division with 0, please choose non zero numbers')
 
 calculator()
